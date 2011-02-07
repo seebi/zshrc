c8a98529d1a0c0416cf75e5e66e0752f51712c96
Sebastian Tramp <none@none>
2011-02-07 17:42:37 +0100

    add: docu head

--- darwin.zsh
+++ darwin.zsh
@@ -1,3 +1,8 @@
+# @author     Philipp Frischmuth <frischmuth@informatik.uni-leipzig.de>
+# @license    http://opensource.org/licenses/gpl-license.php
+#
+# darwin specific fixes / alignments
+
 # Darwin ls command does not support --color option.
 alias ls='ls'
 
