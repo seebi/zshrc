bfe3b915268b4a410313ce0f362b16f9d10f7692
Sebastian Tramp <mail@sebastian.tramp.name>
2012-08-08 13:15:19 +0200

    add alt-left, fn-left/right keybinds

--- options.zsh
+++ options.zsh
@@ -11,6 +11,12 @@ bindkey "^[[1;5D" .backward-word
 bindkey "^[[1;5C" .forward-word
 bindkey "^[[1;6D" backward-delete-word
 bindkey "^[[1;6C" delete-word
+# alt+left (on mac) deletes word
+bindkey "^[" backward-kill-word
+# fn-left
+bindkey "^[[H" .backward-word
+# fn-right
+bindkey "^[[F" .forward-word
 
 # arrow up/down searches in history if line is already started
 bindkey '^[[A' up-line-or-search
