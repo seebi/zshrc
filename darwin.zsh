a662c35cbc34029c84159fa02bf0966c41243824
Sebastian Tramp <mail@sebastian.tramp.name>
2012-08-21 10:31:06 +0200

    add support for dircolors in macos with brew coreutils

--- darwin.zsh
+++ darwin.zsh
@@ -5,8 +5,22 @@
 
 # Darwin ls command does not support --color option.
 alias ls=' ls'
-alias myls=' ls'
+#alias myls=' ls'
+# use gnu ls instead of bsd ls
+alias myls=' gls -C -F -h --color=always'
 
 # The OSX way for ls colors.
 export CLICOLOR=1
 export LSCOLORS="gxfxcxdxbxegedabagacad"
+
+# activate gls colors
+if [[ -a $HOME/.dir_colors ]]; then
+    if [[ "$TERM" == *256* ]]; then
+        which gdircolors > /dev/null && eval "`gdircolors -b $HOME/.dir_colors`"
+    else
+        # standard colors for non-256-color terms
+        which gdircolors > /dev/null && eval "`gdircolors -b`"
+    fi
+else
+    which gdircolors > /dev/null && eval "`gdircolors -b`"
+fi
