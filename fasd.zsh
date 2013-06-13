8bf2a9bcdb8834c745f5efbf17f7bbf692e072e3
Sebastian Tramp <mail@sebastian.tramp.name>
2013-06-14 01:41:32 +0200

    add fasd config/init

---
+++ fasd.zsh
@@ -0,0 +1,25 @@
+# @author     Sebastian Tramp <mail@sebastian.tramp.name>
+# @license    http://opensource.org/licenses/gpl-license.php
+#
+# fasd integration
+# https://github.com/clvv/fasd
+#
+
+# init fasd, the autojump successor
+#eval "$(fasd --init auto)"
+eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
+
+# fasd_cd + outputs directory
+fasd_cd() {
+  if [ $# -le 1 ]; then
+    fasd "$@"
+  else
+    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
+    [ -z "$_fasd_ret" ] && return
+    [ -d "$_fasd_ret" ] && print "$_fasd_ret" && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
+  fi
+}
+
+# as an autojump user, i need only this for now
+alias j=' fasd_cd -d'
+
