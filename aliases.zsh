a94c3aa8e471c0560390b407c3d833f98b673b6a
Sebastian Tramp <sebastian.tramp@eccenca.com>
2020-06-30 00:18:19 +0200

    add docker alias

--- aliases.zsh
+++ aliases.zsh
@@ -9,6 +9,8 @@ export EDITOR="nvim"
 # http://superuser.com/questions/403355/how-do-i-get-searching-through-my-command-history-working-with-tmux-and-zshell
 bindkey -e
 
+alias d="docker"
+
 alias vi="nvim"
 alias vim="nvim"
 alias aedit=" $EDITOR $ZSH_CONFIG/aliases.zsh; source $ZSH_CONFIG/aliases.zsh"
